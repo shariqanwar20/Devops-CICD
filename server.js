4b5f0af10e493660af779cc30c097007725a015c
shariqanwar20 <shariq.anwar@nixorcollege.edu.pk>
2024-04-12 19:42:51 +0500

    updated log message

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const PORT = 3000;
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Guys!!');
+  res.send('Hello Ammar bhai!!');
 });
 app.listen(PORT);
 console.log(`Running on ${PORT}`);
